949c7174ed7f931b52360c669239605468a22ce4
Sinisterdeath <jye.goodlet@gmail.com>
2015-09-18 11:50:14 +1000

    initial seed of asp mvc

---
+++ Connect2Fit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Connect2Fit.MvcApplication" Language="C#" %>
---
+++ Connect2Fit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Connect2Fit.Startup))]
+namespace Connect2Fit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
