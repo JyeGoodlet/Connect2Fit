ba552ea76269903e894ce191425162e95b32c9be
jgoodlet <jye.goodlet@gmail.com>
2015-09-20 13:38:31 +1000

    Start at creating our own login system.

---
+++ Connect2Fit/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Connect2Fit.Models;
+
+namespace Connect2Fit.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult ClientSignUp()
+        {
+
+            return View();
+        }
+
+        //idea is to have a seperate sign up for clients and instructors
+        [HttpPost]
+        public ActionResult ClientSignUp(SignupView signup)
+        {
+
+            throw new NotImplementedException();
+
+        }
+    }
+}
---
+++ Connect2Fit/Models/UserViewModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Connect2Fit.Models
+{
+    public class SignupView
+    {
+
+        [Required]
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string emailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+
+        [Required]
+        [Display(Name = "Password Confirm")]
+        [DataType(DataType.Password)]
+        [Compare("password", ErrorMessage = "Passwords do not match")]
+        public string passwordConfirm { get; set; }
+
+        
+
+
+    }
+
+
+    
+}
