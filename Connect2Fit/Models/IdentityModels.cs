e877ae87f632b449da0ae3e32f81f7d13e667046
justinkhart <justin.k.hart@gmail.com>
2015-09-27 20:43:59 +1000

    Setup project to connect to hosted mssql in Azure. Renamed db in project from 'DefaultConnection' to 'DB_Connect2Fit'

--- Connect2Fit/Models/IdentityModels.cs
+++ Connect2Fit/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Connect2Fit.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DB_Connect2Fit", throwIfV1Schema: false)
         {
         }
 
