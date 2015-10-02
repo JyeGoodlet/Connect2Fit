8e3910576a46cd0c3ae1ed075fe5e62d2e43831c
jgoodlet <jye.goodlet@gmail.com>
2015-10-02 23:23:02 +1000

    testing signalr on the server.

--- Connect2Fit/Startup.cs
+++ Connect2Fit/Startup.cs
@@ -9,6 +9,7 @@ namespace Connect2Fit
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
---
+++ Connect2Fit/hubs/ScheduleItemHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Connect2Fit.hubs
+{
+    public class ScheduleItemHub : Hub
+    {
+        public void HelloServer()
+        {
+            Clients.All.helloClient();
+        }
+    }
+}
