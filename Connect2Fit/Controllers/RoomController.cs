a84749be6bb2b47086b2cbf3e4e9a5ad76e5398e
Sinisterdeath <jye.goodlet@gmail.com>
2015-10-07 17:01:35 +1100

    Researching webrtc.

--- Connect2Fit/Controllers/RoomController.cs
+++ Connect2Fit/Controllers/RoomController.cs
@@ -11,8 +11,27 @@ namespace Connect2Fit.Controllers
         // GET: Room
         public ActionResult Index(int id)
         {
-
             return View(id);
         }
+
+        //after the views have been created this can be merged into a single Room action. with different views
+        public ActionResult InstructorRoom(int id)
+        {
+            return View(id);
+        }
+
+        public ActionResult ClientRoom(int id)
+        {
+            return View(id);
+        }
+
+        public ActionResult WebRtcTest(int id = 1)
+        {
+            return View();
+
+        }
+
+
+
     }
 }
