1482595fb7fbd3050290f38b4ebceb1738388031
Sinisterdeath <jye.goodlet@gmail.com>
2015-11-12 15:55:04 +1100

    trying signalr service bus for scaling out.

--- Connect2Fit/Startup.cs
+++ Connect2Fit/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
 using Owin;
+using System.Configuration;
 
 [assembly: OwinStartupAttribute(typeof(Connect2Fit.Startup))]
 namespace Connect2Fit
@@ -9,6 +11,8 @@ namespace Connect2Fit
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            GlobalHost.DependencyResolver.UseServiceBus(ConfigurationManager.ConnectionStrings["Connect2FitServiceBus"].ConnectionString, "Chat");
             app.MapSignalR();
         }
     }
