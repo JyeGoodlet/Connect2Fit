531519474ae5beb322af07f1f3bbee7b38105782
Sinisterdeath <jye.goodlet@gmail.com>
2015-11-12 16:53:40 +1100

    trying signalr scaleout using sql

--- Connect2Fit/Startup.cs
+++ Connect2Fit/Startup.cs
@@ -11,7 +11,8 @@ namespace Connect2Fit
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-
+            string sqlConnectionString = "Server=tcp:f5bbrkoevs.database.windows.net,1433;Database=connect2fit;User ID=jye.goodlet@f5bbrkoevs;Password=RandomPassword123;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            GlobalHost.DependencyResolver.UseSqlServer(sqlConnectionString);
             app.MapSignalR();
         }
     }
