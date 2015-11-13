59d4a4d04c7e4e05408f2298a5ec1f67385dbf56
jgoodlet <jye.goodlet@gmail.com>
2015-11-13 21:31:26 +1100

    azure using utc for datetime.now. testing a fix for the room time check

--- Connect2Fit/Controllers/RoomController.cs
+++ Connect2Fit/Controllers/RoomController.cs
@@ -35,7 +35,8 @@ namespace Connect2Fit.Controllers
 
             //Check if room can be entered yet. (hour before and hour after block)
             //also check if room exists
-            if (scheduleItem == null || DateTime.Now > scheduleItem.ClassDateTime.AddHours(1) || DateTime.Now < scheduleItem.ClassDateTime.AddHours(-1) || scheduleItem.sessionEnded)
+            var test = scheduleItem.ClassDateTime.ToUniversalTime();
+            if (scheduleItem == null || DateTime.UtcNow > scheduleItem.ClassDateTime.ToUniversalTime().AddHours(1) || DateTime.UtcNow < scheduleItem.ClassDateTime.ToUniversalTime().AddHours(-1) || scheduleItem.sessionEnded)
             {
 
 
