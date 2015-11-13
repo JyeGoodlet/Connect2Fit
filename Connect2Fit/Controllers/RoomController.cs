efa2356caac34b08b6d5c214b6421faadfc6e001
jgoodlet <jye.goodlet@gmail.com>
2015-11-13 21:46:54 +1100

    just going to remove this for now.

--- Connect2Fit/Controllers/RoomController.cs
+++ Connect2Fit/Controllers/RoomController.cs
@@ -35,7 +35,8 @@ namespace Connect2Fit.Controllers
 
             //Check if room can be entered yet. (hour before and hour after block)
             //also check if room exists
-            if (scheduleItem == null || DateTime.Now.ToLocalTime() > scheduleItem.ClassDateTime.ToLocalTime().AddHours(1) || DateTime.Now.ToLocalTime() < scheduleItem.ClassDateTime.ToLocalTime().AddHours(-1) || scheduleItem.sessionEnded)
+            
+            if (/*scheduleItem == null || DateTime.Now.ToLocalTime() > scheduleItem.ClassDateTime.ToLocalTime().AddHours(1) || DateTime.Now.ToLocalTime() < scheduleItem.ClassDateTime.ToLocalTime().AddHours(-1) ||*/ scheduleItem.sessionEnded)
             {
 
                 //redirect to my classes for now
