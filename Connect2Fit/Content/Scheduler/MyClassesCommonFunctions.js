671f5c9b15b87e7b73a75e417c30087abc2b9d8e
jgoodlet <jye.goodlet@gmail.com>
2015-11-20 00:39:44 +1100

    Cleaned up javascript code a little bit. Added Loading and no classes indicator to scheduler

---
+++ Connect2Fit/Content/Scheduler/MyClassesCommonFunctions.js
@@ -0,0 +1,33 @@
+﻿angular.module('schedule.commonFunctions', []).
+   service('commonFunctions', function () {
+
+
+       this.progressbarColor = function (classItem) {
+
+           if (classItem.attendiesCount / classItem.maxAttendies * 100 <= 50) {
+               return "progress-bar-success";
+           }
+           else if (classItem.attendiesCount / classItem.maxAttendies * 100 > 50 && classItem.attendiesCount / classItem.maxAttendies * 100 < 80) {
+               return "progress-bar-warning";
+           }
+           else if (classItem.attendiesCount / classItem.maxAttendies * 100 >= 80) {
+               return "progress-bar-danger";
+           }
+       }
+
+
+       this.nearTime = function (date, time) {
+           moment.locale('en-AU');
+           classDateTime = moment(date + " " + time, "DD-MM-YYYY HH:mm A");
+           if (classDateTime.isBetween(moment().subtract(1, 'hours'), moment().add(1, 'hours'))) {
+               return true;
+           }
+           else {
+               return false;
+           }
+
+       }
+
+
+
+   })
