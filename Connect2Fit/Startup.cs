26492ff47dea4d7dd486da206ed334f4f50aacbc
jgoodlet <jye.goodlet@gmail.com>
2015-11-22 14:31:50 +1100

    changing name of signalr scaleout topic name.

--- Connect2Fit/Startup.cs
+++ Connect2Fit/Startup.cs
@@ -14,13 +14,15 @@ namespace Connect2Fit
             string connectionString = "Endpoint=sb://connect2fit.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=WPxH/8w8B2Mqx47PfU6H2h8dhQeLwXWzb7L1E6CHZDU=";
 
 
-            var config = new ServiceBusScaleoutConfiguration(connectionString, "blah")
+            //remove me to disable signalr scaleout
+            var config = new ServiceBusScaleoutConfiguration(connectionString, "Scaleout")
             {
                 TopicCount = 3,
                 MaxQueueLength = 50
             };
             GlobalHost.DependencyResolver.UseServiceBus(config);
             app.MapSignalR();
+            //end here
         }
     }
 }
