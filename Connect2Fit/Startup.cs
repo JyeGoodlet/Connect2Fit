e6488bfe7196f18e4a1891223785325805ecd812
jgoodlet <jye.goodlet@gmail.com>
2015-11-22 14:39:33 +1100

    moving service bus code to web.config file. Adding comment to startup to indicate what code must be remove to disable service bus.

--- Connect2Fit/Startup.cs
+++ Connect2Fit/Startup.cs
@@ -11,18 +11,17 @@ namespace Connect2Fit
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            string connectionString = "Endpoint=sb://connect2fit.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=WPxH/8w8B2Mqx47PfU6H2h8dhQeLwXWzb7L1E6CHZDU=";
-
-
             //remove me to disable signalr scaleout
+            string connectionString =  ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"]; 
             var config = new ServiceBusScaleoutConfiguration(connectionString, "Scaleout")
             {
                 TopicCount = 3,
                 MaxQueueLength = 50
             };
             GlobalHost.DependencyResolver.UseServiceBus(config);
-            app.MapSignalR();
             //end here
+            app.MapSignalR();
+     
         }
     }
 }
